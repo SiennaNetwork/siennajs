ff9370df1870934fcdb2c35b47b693858a7ecb18
Wiz1991 <wizoftime92@gmail.com>
2022-06-20 09:46:43 +0200

    add all messages to launchpad/ido clients

--- SiennaLaunch.ts
+++ SiennaLaunch.ts
@@ -1,32 +1,83 @@
-import {
-    Address,
-    Client,
-    Coin,
-    Duration,
-    Fee,
-    ContractLink,
-    Moment,
-    Uint128,
-} from '@fadroma/client';
+import { Address, Client, ContractLink, Uint128 } from '@fadroma/client';
 import { ViewingKey, ViewingKeyClient } from '@fadroma/client-scrt';
-import {
-    CustomToken,
-    Snip20,
-    Token,
-    TokenKind,
-    getTokenKind,
-} from '@fadroma/tokens';
+import { Snip20 } from '@fadroma/tokens';
+import { AuthMethod } from './Auth';
 
 export class Launchpad extends Client {
-    fees = {
-        lock_snip20: new Fee('350000', 'uscrt'),
-        lock_native: new Fee('280000', 'uscrt'),
-        unlock_native: new Fee('280000', 'uscrt'),
-        unlock_snip20: new Fee('350000', 'uscrt'),
-    };
+    /**
+     * Creates a new project
+     * @param settings
+     * @param entropy
+     * @returns
+     */
+    async launch(settings: IdoSettings, entropy: String) {
+        return this.execute({ launch: { settings, entropy } });
+    }
+
+    /**
+     * Admin only transaction to add new creators
+     * @param addresses List of users
+     *
+     */
+    async add_creators(addresses: Address[]) {
+        return this.execute({ add_project_owners: { addresses } });
+    }
+
+    /**
+     *
+     * Get the entries for a list of users
+     * @param auth Authentication method
+     * @param addresses List of HumanAddr's to be checked
+     * @param time Current timestamp
+     * @returns Entries
+     */
+    async get_entries(
+        auth: AuthMethod<LaunchpadPermissions>,
+        addresses: Address[],
+        time: number
+    ): Promise<number[]> {
+        return this.query({ get_entries: { auth, addresses, time } });
+    }
+    async sale_constraints(): Promise<SaleConstraints> {
+        return this, this.query({ sale_constraints: {} });
+    }
+    async get_idos(
+        start: number = 0,
+        limit: number = 5
+    ): Promise<IdoCollection> {
+        return this.query({ idos: { pagination: { start, limit } } });
+    }
 }
 
-export class IDO extends Client {}
+export class IDO extends Client {
+    async deposit(callback: CallbackMsgType, amount: Uint128, token: Address) {
+        return this.agent
+            .getClient(Snip20, token)
+            .withFee(this.getFee('deposit'))
+            .send(amount, this.address, callback);
+    }
+    async claim_tokens(recipient?: Address) {
+        return this.execute({ claim_tokens: { recipient } });
+    }
+    async refund_tokens(return_type: ReturnTokenType, address?: Address) {
+        return this.execute({ refund_tokens: { address, return_type } });
+    }
+    async sale_info(): Promise<Project> {
+        return this.query({ sale_info: {} });
+    }
+    async sale_status(): Promise<SaleStatus> {
+        return this.query({ sale_status: {} });
+    }
+    async account(auth: AuthMethod<IdoPermissions>): Promise<Account> {
+        return this.query({ account: { auth } });
+    }
+    async eligibility(
+        address: Address,
+        auth: MerkleAuth
+    ): Promise<Eligibility> {
+        return this.query({ eligibility: { address, auth } });
+    }
+}
 
 interface SaleConfig {
     max_allocation: Uint128;
@@ -123,4 +174,33 @@ enum LaunchpadPermissions {
 interface Tier {
     entries: number;
     amount: Uint128;
+}
+
+interface IdoCollection {
+    entries: ContractLink[];
+    total: number;
+}
+interface Project {
+    token_coonfig: TokenConfig;
+    sale_config: SaleConfig;
+    schedule?: SaleSchedule;
+}
+interface SaleSchedule {
+    start: number;
+    duration: number;
+}
+interface SaleStatus {
+    total_allocation: Uint128;
+    available_for_sale: Uint128;
+    prelocked: Uint128;
+    launched?: number;
+}
+interface Account {
+    owner: Address;
+    total_bought: Uint128;
+    pre_lock_amount: Uint128;
+}
+interface Eligibility {
+    eligible: boolean;
+    address: Address;
 }
