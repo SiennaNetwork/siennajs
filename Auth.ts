e0de79384462b4a560c6270057046334df5ebcd5
ivanp <ivan@nignite.com>
2022-06-27 12:48:31 +0200

    Updated Auth client

--- Auth.ts
+++ Auth.ts
@@ -1,66 +1,105 @@
-import { Client, Address } from "@fadroma/client"
-import { Permit, Signer, ViewingKey } from '@fadroma/client-scrt'
+import { Client, Address, ContractLink } from "@fadroma/client";
+import { Permit, Signer, ViewingKey } from "@fadroma/client-scrt";
+import { Pagination } from "./Pagination";
 
 export type AuthStrategy =
-  | { type: 'permit', signer: Signer }
-  | { type: 'vk', viewing_key: { address: Address, key: ViewingKey } }
+  | { type: "permit"; signer: Signer }
+  | { type: "vk"; viewing_key: { address: Address; key: ViewingKey } };
 
-export type AuthMethod <T> =
+export type AuthMethod<T> =
   | { permit: Permit<T> }
-  | { viewing_key: { address: Address, key: ViewingKey } }
+  | { viewing_key: { address: Address; key: ViewingKey } };
 
 export class MockAuthClient extends Client {
-
-  async update (second_contract: any) {
-    return this.execute({ update: { second_contract } })
+  async update(second_contract: any) {
+    return this.execute({ update: { second_contract } });
   }
-
 }
 
 export class AuthProvider extends Client {
-
-  async createGroup (name: any, members: any) {
-    return this.execute({ create_group: { name, members } })
+  async getOracle() {
+    return this.query({ oracle: {} });
   }
 
-  async getGroup (name: string) {
-    return this.query({ group: { name } })
+  // async getAdmin() {
+  //   return this.query({ admin: { admin: {} } });
+  // }
+
+  async createGroup(name: string, members: ContractLink[], admin?: Address) {
+    return this.execute({ create_group: { name, members } });
   }
 
-  async getOracle () {
-    return this.query({ oracle: {} })
+  async addMembers(groupId: number, members: ContractLink[]) {
+    return this.execute({ create_group: { name, members } });
   }
 
-  async getAdmin () {
-    return this.query({ admin: { admin: {} } })
+  async removeMembers(
+    groupId: number,
+    members: ContractLink[],
+    regenerateKey: boolean
+  ) {}
+
+  async swapMembers(
+    groupId: number,
+    oldMember: ContractLink[],
+    newMember: ContractLink[],
+    regenerateKey: boolean
+  ) {
+    return this.execute({
+      swap_members: {
+        group_id: groupId,
+        old_member: oldMember,
+        new_member: newMember,
+        regenerate_key: regenerateKey,
+      },
+    });
   }
 
+  async removeGroup(groupId: number) {
+    return this.execute({ remove_group: { group_id: groupId } });
+  }
+
+  async getGroups(pagination: Pagination) {
+    return this.query({ groups: { pagination } });
+  }
+
+  async getGroup(id: number) {
+    return this.query({ group: id });
+  }
+
+  async findInGroups(target: ContractLink, groups: number[]) {
+    return this.query({ find_in_groups: { target, groups } });
+  }
+
+  async getLogs(pagination: Pagination) {
+    return this.query({ logs: { pagination } });
+  }
 }
 
 export class Auth {
-
-  static viewing_key (address: Address, key: ViewingKey): Auth {
-    return new this({ type: 'vk', viewing_key: { address, key } })
+  static viewing_key(address: Address, key: ViewingKey): Auth {
+    return new this({ type: "vk", viewing_key: { address, key } });
   }
 
-  static permit (signer: Signer): Auth {
-    return new this({ type: 'permit', signer })
+  static permit(signer: Signer): Auth {
+    return new this({ type: "permit", signer });
   }
 
-  constructor (
-    readonly strategy: AuthStrategy
-  ) {}
+  constructor(readonly strategy: AuthStrategy) {}
 
-  async createMethod <T> (address: Address, permission: T): Promise<AuthMethod<T>> {
-    if (this.strategy.type === 'permit') {
+  async createMethod<T>(
+    address: Address,
+    permission: T
+  ): Promise<AuthMethod<T>> {
+    if (this.strategy.type === "permit") {
       const permit = await this.strategy.signer.sign({
         permit_name: `SiennaJS permit for ${address}`,
         allowed_tokens: [address],
         permissions: [permission],
-      })
-      return { permit }
+      });
+      return { permit };
     } else {
-      return { viewing_key: this.strategy.viewing_key }
+      return { viewing_key: this.strategy.viewing_key };
     }
   }
 }
