1cea40cfba340755ba27a28b2e9c5af613e7aaff
Adam A <adam@hack.bg>
2022-07-19 15:27:31 +0300

    fix: imports; disable eslint autofix (ew)

--- Multicall.ts
+++ Multicall.ts
@@ -1,27 +1,27 @@
-import { Address, Client } from '@hackbg/fadroma';
+import { Address, Client } from '@fadroma/client';
 
 export class Multicall extends Client {
-    async version(): Promise<MulticallVersion> {
-        return this.query({ version: {} });
-    }
+  async version(): Promise<MulticallVersion> {
+    return this.query({ version: {} });
+  }
 
-    async multiChain(queries: MultiQuery[]) {
-        return this.query({ multi_chain: { queries } });
-    }
+  async multiChain(queries: MultiQuery[]) {
+    return this.query({ multi_chain: { queries } });
+  }
 }
 
 export interface MultiQuery {
-    contract_address: Address;
-    query: String;
+  contract_address: Address;
+  query: string;
 }
 
 export interface MultiQueryResult {
-    error?: string;
-    data?: String;
+  error?: string;
+  data?: string;
 }
 
 export type ChainResponse = MultiQueryResult[];
 
 export enum MulticallVersion {
-    'v0.0.1',
+  'v0.0.1',
 }
