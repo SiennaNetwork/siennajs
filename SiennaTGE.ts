79f96cc4c9f24ac59ffc795145323259c8be14c1
Adam A <adam@hack.bg>
2022-09-25 02:42:39 +0300

    FIX(tge): deployment class methods

--- SiennaTGE.ts
+++ SiennaTGE.ts
@@ -18,19 +18,21 @@ import {
 /** Connect to an existing TGE. */
 export default class SiennaTGE extends Deployment {
 
+  log = log as any
+
   names = { token: 'SIENNA', mgmt: 'SIENNA.MGMT', rpt: 'SIENNA.RPT' }
 
   /** The deployed SIENNA SNIP20 token contract. */
   token = this.contract({ name: this.names.token, client: Snip20 }).get()
 
   /** Get the balance of an address in the vested token. */
-  getBalance = async (addr: Address, vk: ViewingKey) => {
+  async getBalance (addr: Address, vk: ViewingKey) {
     this.log.info(`Querying balance of ${addr}...`)
     return await (await this.token).getBalance(addr, vk)
   }
 
   /** Set the VK of the calling address in the vested token. */
-  setVK = async (vk: ViewingKey) => {
+  async setVK (vk: ViewingKey) {
     this.log.info('Setting VK...')
     return await (await this.token).vk.set(vk)
   }
@@ -40,16 +42,19 @@ export default class SiennaTGE extends Deployment {
   mgmt = this.contract({ name: this.names.mgmt, client: MGMT_TGE }).get()
 
   /** Fetch the current schedule of MGMT. */
-  getMgmtSchedule = () =>
-    this.mgmt.then(mgmt=>mgmt.schedule())
+  getSchedule () {
+    return this.mgmt.then(mgmt=>mgmt.schedule())
+  }
 
   /** Fetch the current schedule of MGMT. */
-  getMgmtStatus = () =>
-    this.mgmt.then(mgmt=>mgmt.status())
+  getMgmtStatus () {
+    return this.mgmt.then(mgmt=>mgmt.status())
+  }
 
   /** Fetch the current progress of the vesting. */
-  getMgmtProgress = (addr: Address) =>
-    this.mgmt.then(mgmt=>mgmt.progress(addr))
+  getMgmtProgress (addr: Address) {
+    return this.mgmt.then(mgmt=>mgmt.progress(addr))
+  }
 
   /** Show the current progress of the vesting. */
   /** The deployed RPT contract, which claims tokens from MGMT
@@ -57,55 +62,45 @@ export default class SiennaTGE extends Deployment {
   rpt = this.contract({ name: this.names.rpt, client: RPT_TGE }).get()
 
   /** Update the RPT configuration. */
-  setRptConfig = (config: RPTConfig) => console.warn('SiennaTGE#setRptConfig: TODO')
+  setRptConfig (config: RPTConfig) {
+    console.warn('SiennaTGE#setRptConfig: TODO')
+  }
 
   /** Fetch the current status of RPT. */
-  getRptStatus = () => this.rpt.then(rpt=>rpt.status())
+  getRptStatus () {
+    return this.rpt.then(rpt=>rpt.status())
+  }
 
   showStatus = this.command('status', 'show the status of this TGE', async (
     address = this.agent?.address!
   ) => {
     await this.showMgmtStatus()
+      .catch(({message})=>log.error("Can't show MGMT status:  ", message))
     await this.showMgmtProgress(address)
+      .catch(({message})=>log.error("Can't show MGMT progress:", message))
     await this.showRptStatus()
+      .catch(({message})=>log.error("Can't show RPT status:   ", message))
   })
 
   /** Print the result of getBalance. */
-  showBalance = async (addr: Address, vk: ViewingKey) => {
-    try {
-      log.balance(addr, await this.getBalance(addr, vk))
-    } catch (e) {
-      if (this.isMainnet) {
-        this.log.error('Mainnet: no VK?')
-        return
-      }
-      const VK = await this.setVK(vk)
-      log.balance(addr, await this.getBalance(addr, vk))
-    }
+  async showBalance (addr: Address, vk: ViewingKey) {
+    log.balance(addr, await this.getBalance(addr, vk))
   }
   /** Show the current status of the MGMT */
-  showMgmtStatus = async () => {
-    try {
-      const {address} = await this.mgmt
-      const status = await this.getMgmtStatus()
-      log.mgmtStatus(status)
-      return status
-    } catch (e) {
-      log.error((e as Error).message)
-    }
+  async showMgmtStatus () {
+    const {address} = await this.mgmt
+    const status = await this.getMgmtStatus()
+    log.mgmtStatus(status)
+    return status
   }
-  showMgmtProgress = async (user: Address) => {
-    try {
-      const {address} = await this.mgmt
-      const progress = await this.getMgmtProgress(user)
-      log.mgmtProgress(user, address, progress)
-      return progress
-    } catch (e) {
-      log.error((e as Error).message)
-    }
+  async showMgmtProgress (user: Address) {
+    const {address} = await this.mgmt
+    const progress = await this.getMgmtProgress(user)
+    log.mgmtProgress(user, address, progress)
+    return progress
   }
   /** Show the current status of the RPT. */
-  showRptStatus = async () => {
+  async showRptStatus () {
     const status = await (await this.rpt).status() as { config: any[] }
     log.rptStatus(this.rpt, status)
     log.rptRecipients((await Promise.all(status.config.map(
@@ -307,11 +302,11 @@ export class RPT_TGE extends RPT {
 
   /** Generate an init message for original RPT */
   static init = (
-    admin:    Address,
-    portion:  RPTAmount,
-    config:   RPTConfig,
-    token:    IntoLink,
-    mgmt:     IntoLink
+    admin:   Address,
+    portion: RPTAmount,
+    config:  RPTConfig,
+    token:   IntoLink,
+    mgmt:    IntoLink
   ) => ({
     admin,
     portion,
