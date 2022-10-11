aaf230c4a29f5158ac925d471f46025ce1e778fb
Adam A <adam@hack.bg>
2022-10-11 03:34:11 +0300

    FIX(ci): esm bundle name

--- .rollup-esm.config.mjs
+++ .rollup-esm.config.mjs
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import pkg from './package.json' assert { type: 'json' }
 
-export const file = `bundles/${pkg.name}-${pkg.version}.cjs.bundle.js`
+export const file = `bundles/${pkg.name}-${pkg.version}.esm.bundle.js`
 
 export default {
   input: 'index.dist.cjs',
