712727cd42cdeafb6959f97aa046e85ce2459b1a
Adam A <adam@hack.bg>
2023-02-03 07:20:57 +0200

    refactor: add Versions.ts and make all subsystems versioned

---
+++ Versions.ts
@@ -0,0 +1,15 @@
+export type TGEVersion        = 'v1'
+
+export type AuthVersion       = 'v1'
+
+export type AMMVersion        = 'v1' | 'v2'
+
+export type RewardsVersion    = 'v2' | 'v3' | 'v3.1' | 'v4.1'
+
+export type LendVersion       = 'v1'
+
+export type PFRVersion        = 'v1'
+
+export type GovernanceVersion = 'v1'
+
+export type LaunchpadVersion  = 'v1'
--- index.ts
+++ index.ts
@@ -1,74 +1,126 @@
+export * from './Core'
 import { Contract, Deployment } from './Core'
 
-import SiennaTGE,     { SiennaSnip20 }      from './SiennaTGE'
-import SiennaSwap,    { AMMVersion }        from './SiennaSwap'
-import SiennaRewards, { RewardsAPIVersion } from './SiennaRewards'
-import SiennaAuth       from './Auth'
+export * as VestingConfig from './Vesting'
+
+export * from './SiennaTGE'
+import SiennaTGE, { SiennaSnip20 }from './SiennaTGE'
+
+export * from './SiennaPFR'
+import SiennaPFR from './SiennaPFR'
+
+export * from './SiennaSwap'
+import SiennaSwap from './SiennaSwap'
+
+export * from './Auth'
+import SiennaAuth from './Auth'
+
+export * from './SiennaRewards'
+export * from './SiennaRewards_v2'
+export * from './SiennaRewards_v3'
+export * from './SiennaRewards_v4'
+import SiennaRewards from './SiennaRewards'
+
+export * from './Poll'
 import SiennaGovernance from './Poll'
-import SiennaLaunch     from './SiennaLaunch'
-import SiennaLend       from './SiennaLend'
-import SiennaPFR        from './SiennaPFR'
+
+export * from './SiennaLaunch'
+import SiennaLaunch from './SiennaLaunch'
+
+export * from './SiennaLend'
+import SiennaLend from './SiennaLend'
+
+export * from './Multicall'
+
+export * as Versions from './Versions'
+
+import {
+  TGEVersion,
+  AuthVersion,
+  AMMVersion,
+  RewardsVersion,
+  LendVersion,
+  PFRVersion,
+  GovernanceVersion,
+  LaunchpadVersion
+} from './Versions'
 
 export default class Sienna extends Deployment {
 
   /** The SIENNA token. */
-  get token (): Contract<SiennaSnip20> { return this.tge.token }
-
-  /** The SIENNA Token Generation Event. */
-  tge = new SiennaTGE(this as Deployment)
-
-  /** The Sienna Swap AMM. */
-  amm = {
-    v1: new SiennaSwap(this, 'v1'),
-    v2: new SiennaSwap(this, 'v2')
+  get token (): Contract<SiennaSnip20> {
+    return this.tge['v1'].token
   }
 
-  /** The Sienna Rewards staking system. */
-  rewards = {
-    v2:   new SiennaRewards(this, 'v2'),
-    v3:   new SiennaRewards(this, 'v3'),
-    v3_1: new SiennaRewards(this, 'v3.1'),
-    v4_1: new SiennaRewards(this, 'v4.1'),
+  /** API for SIENNA Token Generation Event. */
+  static TGE = SiennaTGE
+
+  /** Deployments of SIENNA Token Generation Event. */
+  tge: Record<TGEVersion, SiennaTGE> = {
+    'v1': new SiennaTGE(this as Deployment)
   }
 
-  /** The Sienna Lend lending platform. */
-  lend   = new SiennaLend(this, 'v1')
+  /** API for SIENNA Authentication Provider. */
+  static Auth = SiennaAuth
 
-  /** Partner-Funded Rewards: vesting of non-SIENNA tokens. */
-  pfr    = new SiennaPFR(this, 'v3.1')
+  /** Deployments of SIENNA Authentication Provider. */
+  auth: Record<AuthVersion, SiennaAuth> = {
+    'v1': new SiennaAuth(this as Deployment)
+  }
 
-  /** Sienna Governance system. */
-  gov    = new SiennaGovernance(this as Deployment)
+  /** API for Sienna Swap. */
+  static Swap = SiennaSwap
 
-  /** Sienna Launch: Launchpad/IDO system. */
-  launch: SiennaLaunch = new SiennaLaunch(this as Deployment)
+  /** Deployments of Sienna Swap. */
+  amm: Record<AMMVersion, SiennaSwap> = {
+    'v1': new SiennaSwap(this, 'v1'),
+    'v2': new SiennaSwap(this, 'v2')
+  }
 
-  static Auth       = SiennaAuth
-  static TGE        = SiennaTGE
-  static Swap       = SiennaSwap
-  static Rewards    = SiennaRewards
-  static Lend       = SiennaLend
-  static PFR        = SiennaPFR
+  /** API for Sienna Rewards. */
+  static Rewards = SiennaRewards
+
+  /** Deployments of Sienna Rewards. */
+  rewards: Record<RewardsVersion, SiennaRewards> = {
+    'v2':   new SiennaRewards(this, 'v2'),
+    'v3':   new SiennaRewards(this, 'v3'),
+    'v3.1': new SiennaRewards(this, 'v3.1'),
+    'v4.1': new SiennaRewards(this, 'v4.1'),
+  }
+
+  /** API for Sienna Lend */
+  static Lend = SiennaLend
+
+  /** Deployments of Sienna Lend. */
+  lend: Record<LendVersion, SiennaLend> = {
+    'v1': new SiennaLend(this, 'v1')
+  }
+
+  /** API for Partner-Funded Rewards. */
+  static PFR = SiennaPFR
+
+  /** Deployments of Partner-Funded Rewards. */
+  pfr: Record<PFRVersion, SiennaPFR> = {
+    'v1': new SiennaPFR(this, 'v3.1')
+  }
+
+  /** API for Sienna Governance. */
   static Governance = SiennaGovernance
-  static Launch     = SiennaLaunch
+
+  /** Deployments of Sienna Governance. */
+  gov: Record<GovernanceVersion, SiennaGovernance> = {
+    'v1': new SiennaGovernance(this as Deployment)
+  }
+
+  /** API for Sienna Launchpad. */
+  static Launch = SiennaLaunch
+
+  /** Deployments of Sienna Launchpad. */
+  launch: Record<LaunchpadVersion, SiennaLaunch> = {
+    'v1': new SiennaLaunch(this as Deployment)
+  }
 
 }
-
-export * from './Core'
-export * from './Auth'
-export * from './Multicall'
-export * from './SiennaTGE'
-export * from './SiennaSwap'
-export * from './SiennaRewards'
-export * from './SiennaRewards_v2'
-export * from './SiennaRewards_v3'
-export * from './SiennaPFR'
-export * from './SiennaLend'
-export * from './SiennaRewards_v4'
-export * from './Poll'
-export * from './SiennaLaunch'
-
-export * as VestingConfig from './Vesting'
 
 export {
   SiennaAuth,
