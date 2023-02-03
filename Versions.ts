0353c891a7c53dcf78b940950eb950963e1ed215
Adam A <adam@hack.bg>
2023-02-03 13:26:32 +0200

    feat+docs(versions): collect and document more definitions/relations of subsystem versions in Versions.ts

--- Versions.ts
+++ Versions.ts
@@ -1,12 +1,31 @@
-export type TGEVersion        = 'v1'
+/** Versions of Sienna TGE
+  * v1: Initial release */
+export type TGEVersion = 'v1'
 
-export type AuthVersion       = 'v1'
+/** Versions of Sienna Auth
+  * v1: Initial release */
+export type AuthVersion = 'v1'
 
-export type AMMVersion        = 'v1' | 'v2'
+/** Versions of Sienna Aut
+  * v1: Initial release */
+export type AuthProviderVersion = 'v1'
 
+export const LatestAuthProviderVersion: AuthProviderVersion = 'v1'
+
+/** Versions of Sienna Swap
+  * v1: what was the bug?
+  * v2: fixes v1 */
+export type AMMVersion = 'v1' | 'v2'
+
+/** Versions of the Rewards system.
+  * Rewards v1: false start.
+  * Rewards v2: buggy distribution algorithm.
+  * Rewards v3: where did it go?
+  * Rewards v3.1: what did it add?
+  * Rewards v4.1: what did it add? */
 export type RewardsVersion    = 'v2' | 'v3' | 'v3.1' | 'v4.1'
 
-/** Which version of AMM corresponds to which version of rewards. */
+/** Which version of AMM corresponds to which version of Rewards. */
 export const RewardsToAMMVersion: Record<RewardsVersion, AMMVersion> = {
   'v2':   'v1',
   'v3':   'v2',
@@ -14,17 +33,29 @@ export const RewardsToAMMVersion: Record<RewardsVersion, AMMVersion> = {
   'v4.1': 'v2',
 };
 
-export type LendVersion       = 'v1'
+/** Which version of Auth Provider is used by which version of Rewards. */
+export const RewardsToAuthVersion: Record<RewardsVersion, AuthVersion|null> = {
+  'v2':   null,
+  'v3':   null,
+  'v3.1': null,
+  'v4.1': 'v1'
+}
 
-export type PFRVersion        = 'v1'
+/** Versions of Sienna Lend
+  * v1: Initial release */
+export type LendVersion = 'v1'
 
+/** Versions of Sienna PFR
+  * v1: Initial release */
+export type PFRVersion = 'v1'
+
+/** Versions of Sienna Governance
+  * v1: Initial release */
 export type GovernanceVersion = 'v1'
 
+/** Versions of Sienna Launch
+  * v1: Initial release */
 export type LaunchpadVersion  = 'v1'
-
-export type AuthProviderVersion = 'v1'
-
-export const LatestAuthProviderVersion: AuthProviderVersion = 'v1'
 
 export enum MulticallVersion {
   'v0.0.1',
